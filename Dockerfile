6181c11a66c66931366f6b0b5f83f5d5aa34156c
Amit Agrawal <family@Amits-MacBook-Pro.local>
2022-05-22 20:53:48 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:8-jre-alpine
+EXPOSE 8080
+COPY ./build/libs/learn-springboot 0.0.1-SNAPSHOT.jar /usr/app/
+WORKDIR /usr/app
+ENTRYPOINT ["java", "-jar", "learn-springboot 0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/learn/springboot/learnspringboot/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.learnspringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Welcome to hello world!!!";
+    }
+}
